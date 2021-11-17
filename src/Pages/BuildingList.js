cc4e44bde2ecc34a4462aea4c3769ef873744fe5
Meghan <meghanmurphy.212@gmail.com>
2021-11-17 11:36:01 -0500

    updated cfa favicon

--- src/Pages/BuildingList.js
+++ src/Pages/BuildingList.js
@@ -197,7 +197,7 @@ const BuildingList = () => {
 
                                            <MDBListGroupItem
                                                onClick = {()=>{handleSetBuilding(value.name); handleSetFloor(floor.name); handleTitle('Floor Plan'); history.push("/floorPlan");}}
-                                               className="d-flex justify-content-between align-items-center floorBtn">{"Floor " + floor.name}
+                                               className="justify-content-between align-items-center floorBtn">{"Floor " + floor.name}
                                            </MDBListGroupItem>))
                                        }
                                    </MDBListGroup>
